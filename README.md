8fca54e36c6761e1b191621c9f1d5d3026dc8c10
neetoons <neet.toons@gmail.com>
2024-12-10 18:41:51 -0400

    add: code, readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # nix-pawn-compiler
 A nix/NixOS package for pawn compiler
+
+## Implementation
+```nix
+environment.systemPackages = [
+    (pkgs.callPackage ./nix-pawn-compiler/pawncc/default.nix {})
+];
+```
---
+++ pawncc/default.nix
@@ -0,0 +1,45 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+pkgs.stdenv.mkDerivation rec {
+  pname = "pawncc";
+  version = "3.10.10";
+  src = pkgs.fetchFromGitHub {
+    owner = "pawn-lang";
+    repo = "compiler";
+    rev = "v${version}";
+    hash = "sha256-UBMfL7GwfVzU+bjCtALZEswoQ2sy1jC4evGORnLfqR8=";
+  };
+
+  cmakeFlags = [
+    "-DBUILD_TESTING=OFF"
+    "-DCMAKE_INSTALL_PREFIX=$out"
+    "-DCMAKE_POLICY_DEFAULT_CMP0025=NEW"
+    "-Wno-dev"
+    "-DCMAKE_INSTALL_PREFIX="
+    "-DCMAKE_SOURCE_DIR=source/compiler" 
+  ];
+
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    cmake
+  ];
+
+  configurePhase = ''
+    cd source/compiler
+    # mkdir build # si descomentar quitara las advertencias amarillas
+    # cd build
+    # cmake .. ${pkgs.lib.concatStringsSep " " cmakeFlags} 
+    cmake ${pkgs.lib.concatStringsSep " " cmakeFlags} 
+  '';
+
+  installPhase = "make";
+
+  fixupPhase = ''
+    mkdir -p $out/bin $out/lib
+    cp -r pawncc $out/bin/pawncc
+    cp -r libpawnc.so $out/lib/libpawnc.so
+  '';
+
+  doCheck = false;
+}
